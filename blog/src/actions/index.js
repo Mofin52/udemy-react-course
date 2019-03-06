cac226463d351990c5479e493d669a17bf41e5ad
Великоречин Александр <velikorechin@msk.i-teco.ru>
2019-03-06 15:29:19 +0300

    blog finished

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,14 +1,30 @@
 import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonplaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
+
 export const fetchPosts = () => async dispatch => {
     const posts = await jsonplaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: posts.data });
 }
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const user = await jsonplaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: user.data });
-});
+};
+
+// MEMOIZATION
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const user = await jsonplaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: user.data });
+// });
