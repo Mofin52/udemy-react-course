4607733e042fbb726fb9b6192c18bc03316d6394
Великоречин Александр <velikorechin@msk.i-teco.ru>
2019-03-06 15:02:03 +0300

    memoization for blog

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,14 @@
+import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonplaceholder';
 
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonplaceholder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    const posts = await jsonplaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: posts.data });
 }
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const user = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: user.data });
+});
