e099df63f6bfc0df1ef76ac817f3065a2fed2323
Великоречин Александр <velikorechin@msk.i-teco.ru>
2019-03-19 13:40:48 +0300

    translate context app ready

---
+++ translate/src/components/Field.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+class Field extends React.Component {
+    render() {
+        return(
+            <div className='ui field'>
+                <label>
+                <LanguageContext.Consumer>
+                    {(value) => value === 'english' ? 'Name' : 'Имя'}    
+                </LanguageContext.Consumer>
+                </label>
+                <input />
+            </div>
+        )
+    }
+}
+
+export default Field;
