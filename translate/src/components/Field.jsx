a7cd9bbb53fc6659e7b4c7964cf9ef860018ffea
Великоречин Александр <velikorechin@msk.i-teco.ru>
2019-03-19 15:25:18 +0300

    translate context app tuned

--- translate/src/components/Field.jsx
+++ translate/src/components/Field.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+
 class Field extends React.Component {
+    
+    static contextType = LanguageContext;
+
     render() {
+        const text = this.context.language === 'english' ? 'Name' : 'Имя';
         return(
             <div className='ui field'>
-                <label>
-                <LanguageContext.Consumer>
-                    {(value) => value === 'english' ? 'Name' : 'Имя'}    
-                </LanguageContext.Consumer>
-                </label>
+                <label>{text}</label>
                 <input />
             </div>
         )
